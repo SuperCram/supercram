4d4ad11d982a5807b5cffb3703724a70edb96d32
SuperCram <supercrambox@gmail.com>
2013-02-04 12:46:16 +0000

    Update supercramClient/src/WorldSprite.py
    Add TAG stuph

--- supercramClient/src/WorldSprite.py
+++ supercramClient/src/WorldSprite.py
@@ -1,12 +1,24 @@
 '''
 WorldSprite superclass, represents world collision boxes, trigger zones
 '''
-import pygame
+import pygame,cereal
 
 class WorldSprite():
     def __init__(self, left, top, width, height):
-        self.collisions = 1
+        self.collisions = True
         self.background = True
         self.image = ''
+        self.type = 0
         self.rect = pygame.Rect(left, top, width, height)
+        self.params = {}
+    def toTag(self):
+        sprite = cereal.TagMap()
+        sprite["aabb"] = cereal.TagArray([cereal.TagInt(self.rect.left), cereal.TagInt(self.rect.top), cereal.TagInt(self.rect.width), cereal.TagInt(self.rect.height)])
+        sprite["collisions"] = cereal.TagBool(self.collisions)
+        sprite["background"] = cereal.TagBool(self.background)
+        tagParams = {}
+        for param in self.params:
+            tagParams[param] = cereal.TagString(self.params[param])
+        sprite["params"] = cereal.TagMap(tagParams)
+        return sprite
         
