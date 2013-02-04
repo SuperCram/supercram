2cb6b4520ae5a3d4581bc7e67fefbe5b33f3b814
SuperCram <supercrambox@gmail.com>
2013-02-04 12:26:39 +0000

    Update supercramClient/src/World.py
    Random Comment goes here

--- supercramClient/src/World.py
+++ supercramClient/src/World.py
@@ -1,13 +1,52 @@
 '''
 World superclass, represents on-screen level and all contents 
 '''
-        
+import cereal   
 class World():
+    def toTag(self):
+        sprites = []
+        
+        for worldSprite in self.clips:
+            sprites.append(worldSprite.toTag())
+        for worldSprite in self.triggers:
+            sprites.append(worldSprite.toTag())
+        for worldSprite in self.backgrounds:
+            sprites.append(worldSprite.toTag())
+        for worldSprite in self.foregrounds:
+            sprites.append(worldSprite.toTag())
+        sprites = cereal.TagArray(sprites)
+        
+        #Player Spawns
+        tagPlayerSpawns = cereal.TagArray()
+        for point in self.playerSpawns:
+            tagPlayerSpawns.tags.append(cereal.TagArray([cereal.TagInt(point[0]), cereal.TagInt(point[1])]))
+        
+        tagMobSpawns = cereal.TagArray()
+        for point in self.mobSpawns:
+            tagMobSpawns.tags.append(cereal.TagArray([cereal.TagInt(point[0]), cereal.TagInt(point[1])]))
+        tagCrateSpawns = cereal.TagArray()
+        for zone in self.crateSpawnZones:
+            tagCrateSpawns.tags.append(cereal.TagArray([cereal.TagInt(zone.top), cereal.TagInt(zone.left), cereal.TagInt(zone.width), cereal.TagInt(zone.height)]))
+        
+        worldTag = cereal.TagMap()
+        worldTag.data["spirtes"] = sprites
+        worldTag.data["playerSpawns"] = tagPlayerSpawns
+        worldTag.data["mobSpawns"] = tagMobSpawns
+        worldTag.data["crateSpawnPoints"] = tagCrateSpawns
+        worldTag.data["gravity"] = cereal.TagFloat(self.gravity)
+        worldTag.data["size"] = cereal.TagArray([cereal.TagInt(self.size[0]), cereal.TagInt(self.size[1])])
+        worldTag.data["enemySpawnDelay"] = cereal.TagInt(self.enemySpawnDelay)
+        
+        return worldTag
+        
     def __init__(self):
-        self.map = [] #WorldSprites
+        self.clips = []
+        self.triggers = []
+        self.backgrounds = []
+        self.foregrounds = []
+        
         self.playerSpawns = [] #Ints, bottom center
         self.mobSpawns = [] #Ints
-        self.mobTriggers = [] #Rects
         self.crateSpawnZones = [] #Rects
         self.players = []
         self.enemies = []
