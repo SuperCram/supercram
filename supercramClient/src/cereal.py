8f429ac37742985d3c60f1a25ee6fb17367ce530
SuperCram <supercrambox@gmail.com>
2013-02-06 10:28:46 +0000

    Update supercramClient/src/cereal.py
    Updated Cereal WEEEEEEGEEEEE

--- supercramClient/src/cereal.py
+++ supercramClient/src/cereal.py
@@ -1,22 +1,26 @@
 from struct import pack,unpack
-def readTag(self, dataStream):
-    tagID = dataStream.readByte()
+def readTag(dataStream, read=True):
+    tagID = int(dataStream.readByte())
+    t = None
     if tagID == 1:
-        return TagByte()
-    if tagID == 2:
-        return TagString()
+        t = TagByte()
+    elif tagID == 2:
+        t = TagString()
     elif tagID == 3:
-        return TagInt()
+        t = TagInt()
     elif tagID == 4:
-        return TagBool()
+        t = TagBool()
     elif tagID == 5:
-        return TagFloat()
+        t = TagFloat()
     elif tagID == 6:
-        return TagArray()
+        t = TagArray([])
     elif tagID == 7:
-        return TagMap()
+        t = TagMap({})
     else:
-        raise ValueError
+        raise ValueError("Unknown Tag by index "+str(tagID))
+    if(read):
+        t.read(dataStream)
+    return t
 class Tag():
     def __init__(self):
         self.id = 0
@@ -27,64 +31,74 @@ class Tag():
 
 class TagByte(Tag):
     def __init__(self, b=0):
-        self.byte = b
+        self.data = b
         self.id = 1
     def read(self, dataStream):
-        self.byte = dataStream.readByte()
+        self.data = dataStream.readByte()
     def write(self,dataStream):
-        dataStream.writeByte(self.byte)
+        dataStream.writeByte(self.data)
+    def __repr__(self):
+        return "TagByte("+str(self.data)+")"
         
 class TagString(Tag):
     def __init__(self, s=""):
-        self.string = s
+        self.data = s
         self.id = 2
     def read(self, dataStream):
-        self.string = dataStream.readString()
+        self.data = dataStream.readString()
     def write(self, dataStream):
-        dataStream.writeString()
+        dataStream.writeString(self.data)
+    def __repr__(self):
+        return "TagString("+self.data+")"
 
 class TagInt(Tag):
     def __init__(self, i=0):
-        self.i = i
+        self.data = i
         self.id = 3
     def read(self, dataStream):
-        self.i = dataStream.readInt()
+        self.data = dataStream.readInt()
     def write(self,dataStream):
-        dataStream.writeInt(self.i)
+        dataStream.writeInt(self.data)
+    def __repr__(self):
+        return "TagInt("+str(self.data)+")"
 
 class TagBool(Tag):
     def __init__(self, b=False):
-        self.bool = b
+        self.data = b
         self.id = 4
     def read(self, dataStream):
-        self.bool = dataStream.readBool()
+        self.data = dataStream.readBool()
     def write(self,dataStream):
-        dataStream.writeBool(self.bool)
+        dataStream.writeBool(self.data)
+    def __repr__(self):
+        return "TagBool("+str(self.data)+")"
 
 class TagFloat(Tag):
     def __init__(self, f=0):
-        self.float = f
+        self.data = f
         self.id = 5
     def read(self, dataStream):
-        self.float = dataStream.readFloat()
+        self.data = dataStream.readFloat()
     def write(self,dataStream):
-        dataStream.writeFloat(self.float)
+        dataStream.writeFloat(self.data)
+    def __repr__(self):
+        return "TagFloat("+str(self.data)+")"
 
 class TagArray(Tag):
     def __init__(self, arr=[]):
-        self.tags = arr
+        self.data = arr
         self.id = 6
     def read(self, dataStream):
         size = dataStream.readInt()
         for i in range(size):
-            newTag = readTag(dataStream)
-            newTag.read(dataStream)
-            self.tags.append(newTag)
+            self.data.append(readTag(dataStream))
     def write(self, dataStream):
-        dataStream.writeInt(len(self.tags))
-        for t in self.tags:
+        dataStream.writeInt(len(self.data))
+        for t in self.data:
             dataStream.writeByte(t.id)
             t.write(dataStream)
+    def __repr__(self):
+        return "TagArray("+str(self.data)+")"
 
 class TagMap(Tag):
     def __init__(self, map={}):
@@ -94,35 +108,43 @@ class TagMap(Tag):
         tagSize = dataStream.readInt()
         for i in range(tagSize):
             key = dataStream.readString()
-            tag = readTag(dataStream)
-            tag.read(dataStream)
-            self.data[key] = tag
+            tagToRead = readTag(dataStream)
+            self.data[key] = tagToRead
     def write(self, dataStream):
         dataStream.writeInt(len(self.data))
         for t in self.data:
             dataStream.writeString(t)
             dataStream.writeByte(self.data[t].id)
             self.data[t].write(dataStream)
+    def __repr__(self):
+        return "TagMap("+str(self.data)+")"
 class DataInputStream:
     def __init__(self, base_stream):
         self.base_stream = base_stream
     def readByte(self):
-        return self.base_stream.read(1)
+        return ord(self.base_stream.read(1))
     def readBytes(self, length):
         return self.base_stream.read(length)
     def readChar(self):
-        return self.unpack('b')
+        f = bytearray(self.readBytes(2))
+        f.reverse()
+        return str(f).decode("UTF-16LE")
     def readBool(self):
         return self.unpack('?')
     def readInt(self):
-        return self.unpack('i', 4)
+        return self.unpack('!i', 4)
     def readFloat(self):
-        return self.unpack('f', 4)
+        return self.unpack('!f', 4)
     def readString(self):
-        length = self.readInt32()
-        return self.unpack(str(length) + 's', length)
+        length = self.readInt()
+        s = ""
+        for l in range(length):
+            c = self.readChar()
+            s += c
+        return s
+        #return self.unpack(str(length) + 's', length)
     def readTag(self):
-        return None
+        return readTag(self)
     def unpack(self, fmt, length = 1):
         return unpack(fmt, self.readBytes(length))[0]
 class DataOutputStream:
@@ -135,7 +157,6 @@ class DataOutputStream:
         ba.reverse()
         self.writeBytes(ba)
     def writeByte(self, value):
-        value = value&255
         self.base_stream.write(chr(value))
     def writeBool(self, value):
         self.pack('?', value)
@@ -150,3 +171,53 @@ class DataOutputStream:
             self.writeChar(value[l])
     def pack(self, fmt, data):
         return self.writeBytes(pack(fmt, data))
+if(__name__=="__main__"):
+    testFile = open("test.db","wb")
+    dos = DataOutputStream(testFile)
+    dos.writeChar('G');
+    dos.writeByte(100);
+    dos.writeBool(False)
+    dos.writeInt(63324)
+    dos.writeFloat(0.5)
+    dos.writeString("FOOBAR")
+    testFile.close()
+    
+    testFile = open("test.db","rb")
+    dis = DataInputStream(testFile)
+    print dis.readChar()
+    print dis.readByte()
+    print dis.readBool()
+    print dis.readInt()
+    print dis.readFloat()
+    print len(dis.readString())
+    testFile.close()
+    
+    
+    g = TagMap({"data":TagArray([TagBool(False), TagInt(12), TagString("APPLE")]), "other":TagArray([TagFloat(0.5), TagByte(124), TagString("sup")])})
+    #g = TagArray([TagBool(False), TagInt(12), TagString("APPLE")])
+    #g = TagMap({"a":TagString("v")})
+    print g
+    testFile = open("test.db","wb")
+    dos = DataOutputStream(testFile)
+    dos.writeByte(g.id)
+    g.write(dos)
+    testFile.close()
+    
+    testFile = open("test.db","rb")
+    dis = DataInputStream(testFile)
+    newTag = readTag(dis)
+    testFile.close()
+    
+    print newTag
+    
+
+
+
+
+
+
+
+
+
+
+
